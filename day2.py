8969b2c56a4e53bb7061e12c7e2051eaae7947d1
mervealgi <mervealgi@gmail.com>
2021-03-02 13:41:58 +0300

    update

--- day2.py
+++ day2.py
@@ -2,3 +2,13 @@
 
 ###LOGICAL OPERATIONS 
 
+a, b = True , False
+print(type(a))
+
+print(a or b) #will be True
+print(a and b) #will be False
+print(not a) #will be False
+print (a != b) #will be True,bcs re not equal
+print(a == b) #will be False, bcs re not equal
+
+
