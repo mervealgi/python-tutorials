f746585645d43d463d8b0778bb3be5c1633cde9d
mervealgi <mervealgi@gmail.com>
2021-03-02 18:51:12 +0300

    beginning of day3

---
+++ day3.py
@@ -0,0 +1,18 @@
+#INTRODUCTION TO PYTHON DAY2
+
+###INPUT #????? USE input INSTEAD OF raw_input
+
+input1 = raw_input("Please enter your favourite city: ") #must be string 
+print(input1)
+print(type(input1))
+
+input2 = int(raw_input("Please enter an integer: ")) #turn to integer
+print(input2) #can not be entered float
+print(type(input2))
+
+
+
+months = 12
+days = 365
+print("1 year is " + str(months) + " months and " + str(days) + " days." )
+
